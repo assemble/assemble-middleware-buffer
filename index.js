0f8f49cd5d8cabb34e3c34e3a3abe697c89d0102
Brian Woodward <brian.woodward@gmail.com>
2014-06-30 13:03:10 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-middleware-buffer <https://github.com/assemble/assemble-middleware-buffer>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
